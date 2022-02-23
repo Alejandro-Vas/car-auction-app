01161a6a1a02cf7e51abe06b10d77b128565af67
Alejandro-Vas <zended@yandex.ru>
2022-02-23 19:52:23 +0300

    feat: FinishTime

--- client/src/components/FinishTime/FinishTime.jsx
+++ client/src/components/FinishTime/FinishTime.jsx
@@ -1,9 +1,28 @@
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
 function FinishTime(props) {
   const { finishTime } = props;
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setNow(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const end = new Date(finishTime);
+  const distance = now - end;
+
+  const SECOND = 1000;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+
+  const minutes = Math.floor((distance % HOUR) / MINUTE);
+  const seconds = Math.floor((distance % MINUTE) / SECOND);
+
   return (
-    <div>{finishTime}</div>
+    // <div>{distance > 0 ? `${minutes}:${seconds}` : 'Аукцион завершен'}</div>
+    <div>{`${minutes}:${seconds}`}</div>
   );
 }
 export default FinishTime;
